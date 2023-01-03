39c5a723fbff6aa02fc6359f3f2f61ec095cfcd4
namvn <namvn131998@gmail.com>
2023-01-03 16:34:46 +0700

    commit:

--- OnlineShop/Controllers/HomeController.cs
+++ OnlineShop/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace OnlineShop.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult Login()
+        {
+            return View();
+        }
     }
 }
